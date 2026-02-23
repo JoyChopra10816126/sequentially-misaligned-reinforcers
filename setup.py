513e723496d9db6c4f109b68ae9f97f30771f060
Joy Chopra <joy.chopra2@gmail.com>
2026-02-23 14:39:42 +0530

    Initial setup and directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sequentially_misaligned_reinforcers",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # list dependencies here
+    python_requires='>=3.8',
+    author="Joy K. Chopra",
+)
